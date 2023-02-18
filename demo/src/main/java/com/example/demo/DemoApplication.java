c04cc6f92b408dbc844bcf93c73a94dce8579bbc
Alberto Rebolledo <alberto.rebolledo.j@gmail.com>
2023-02-18 01:17:59 +0100

    Todo el backend de SpringBoot

---
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,15 @@
+// ESTA ES LA APLICACIÓN QUE EJECUTA TODO
+
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
---
+++ demo/src/main/java/com/example/demo/model/Person.java
@@ -0,0 +1,31 @@
+// ESTE ES EL MODELO QUE SE USA COMO CLASE
+
+package com.example.demo.model;
+
+import java.util.UUID;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import jakarta.validation.constraints.NotBlank;
+
+public class Person {
+	
+	private final UUID id;
+	
+	@NotBlank
+	private final String name; // No puede estar vacío
+	
+	public Person(@JsonProperty("id") UUID id, @JsonProperty("name") String name) {
+		this.id = id;
+		this.name = name;
+	}
+	
+	public UUID getId() {
+		return this.id;
+	}
+	
+	public String getName() {
+		return name;
+	}
+
+}
